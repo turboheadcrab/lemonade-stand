fbd3bc374e60d9f22bb0bdcf047156603202eebb
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2022-04-12 12:57:52 -0400

    Object Literal Enhancements

--- build/index.js
+++ build/index.js
@@ -1,32 +1,59 @@
 "use strict";
 
+var _lemonade;
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var lemonJuice = 3;
-var water = 3;
-var sugar = 1.5;
-var iceCubes = 10;
-var lemonade = {
-  lemonJuice: lemonJuice,
-  water: water,
-  sugar: sugar,
-  iceCubes: iceCubes,
-  calculatePrice: function calculatePrice() {
-    return this.lemonJuice * 0.3 + this.water * .01 + this.sugar * .25 + this.iceCubes * .05 + .75;
-  }
-};
+var lemonade = (_lemonade = {
+  lemonJuice: 3,
+  water: 3,
+  sugar: 1.5,
+  iceCubes: 5
+}, _defineProperty(_lemonade, console.log('Hello World'), 'Hi'), _defineProperty(_lemonade, "calculatePrice", function calculatePrice() {
+  return this.lemonJuice * 0.3 + this.water * .01 + this.sugar * .25 + this.iceCubes * .05 + .75;
+}), _lemonade);
 
 function updateLemonade(lemonade, lemonJuice, water, sugar, iceCubes) {
-  return _objectSpread(_objectSpread({}, lemonade), {}, {
+  return _objectSpread({
     lemonJuice: lemonJuice,
     water: water,
     sugar: sugar,
     iceCubes: iceCubes
-  });
+  }, lemonade);
 }
 
-console.log(updateLemonade(lemonade, 5, 2.5, 3, 7));
+lemonade.water = 8;
+console.log(_objectSpread(_objectSpread({}, lemonade), {}, {
+  water: 5
+}));
+console.log(lemonade);
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, [1, 2, 3]];
+var numbersCopy = [].concat(numbers);
+numbersCopy[10] = _toConsumableArray(numbers[10]);
+numbersCopy[10][0] = 5;
+console.log(numbers);
+console.log(numbersCopy);
+var a = lemonade.water,
+    lemonJuice = lemonade.lemonJuice,
+    sugar = lemonade.sugar,
+    iceCubes = lemonade.iceCubes;
+console.log(a);
+console.log(lemonJuice);
+console.log(sugar);
+console.log(iceCubes);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-let lemonJuice = 3
-let water = 3
-let sugar = 1.5
-let iceCubes = 10
-
 const lemonade = {
-    lemonJuice,
-    water,
-    sugar,
-    iceCubes,
+    lemonJuice: 3,
+    water: 3,
+    sugar: 1.5,
+    iceCubes: 5,
+    [console.log('Hello World')]: 'Hi',
     calculatePrice() {
         return (
             this.lemonJuice * 0.3 +
@@ -21,12 +17,30 @@ const lemonade = {
 
 function updateLemonade(lemonade, lemonJuice, water, sugar, iceCubes) {
     return {
-        ...lemonade,
         lemonJuice,
         water,
         sugar,
-        iceCubes
+        iceCubes,
+        ...lemonade
     }
 }
 
-console.log(updateLemonade(lemonade, 5, 2.5, 3, 7))
+lemonade.water = 8
+
+console.log({ ...lemonade, water: 5 })
+console.log(lemonade)
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, [1, 2, 3]]
+const numbersCopy = [...numbers]
+numbersCopy[10] = [...numbers[10]]
+
+numbersCopy[10][0] = 5
+console.log(numbers)
+console.log(numbersCopy)
+
+let { water: a, lemonJuice, sugar, iceCubes } = lemonade
+
+console.log(a)
+console.log(lemonJuice)
+console.log(sugar)
+console.log(iceCubes)
